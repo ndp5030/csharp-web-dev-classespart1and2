a08a22e01cdecbbccea11b54f64a14b1a25baa0a
Nicholas Pfleegor <ndp5030@gmail.com>
2022-05-05 05:04:50 -0400

    Chapter 5 Exercises Finished but no custom Equals method created

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,9 +7,9 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
-            Student nick = new Student("Nick", 1, 1, 4.0);
-            Student jenelle = new Student("Jenelle", 1, 1, 4.0);
-            Student francesca = new Student("Francesca", 1, 1, 4.0);
+            Student nick = new Student("Nick", 1, 35, 4.0);
+            Student jenelle = new Student("Jenelle", 1, 65, 4.0);
+            Student francesca = new Student("Francesca", 1, 110, 4.0);
             //Console.WriteLine($"The Student class works! \nName: {nick.Name} \nStudent Id: {nick.StudentId} \nCredits: {nick.NumberOfCredits} \nGPA: {nick.Gpa}");
             List<Student> students = new List<Student>();
             students.Add(nick);
@@ -23,8 +23,11 @@ namespace SchoolPractice
 
             foreach (Student student in students)
             {
-                Console.WriteLine($"..........\nName: {student.Name} \nStudent Id: {student.StudentId} \nCredits: {student.NumberOfCredits} \nGPA: {student.Gpa}");
+                Console.WriteLine(student.ToString());
             }
+            nick.AddGrade(15, 0.0);
+            Console.WriteLine(nick.ToString());
+
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -20,6 +20,43 @@ namespace SchoolPractice
             Gpa = gpa;
         }
 
+        public override string ToString()
+        {
+            return $"..........\nName: {Name} \nStudent Id: {StudentId} \nCredits: {NumberOfCredits} \nGPA: {Gpa} \nGrade Level: {GetGradeLevel(NumberOfCredits)}";
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate fields: NumberOfCredits, Gpa
+            double totalCreditPoints = Gpa * NumberOfCredits;
+            totalCreditPoints += courseCredits * grade;
+            NumberOfCredits += courseCredits;
+            Gpa = totalCreditPoints / NumberOfCredits;
+        }
+
+        public string GetGradeLevel(int credits)
+        {
+            // Determine the grade level of the student based on NumberOfCredits
+            //Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits)
+            if (credits <= 29) 
+            {
+                return "Freshman";
+            } else if (credits <= 59)
+            {
+                return "Sophomore";
+            } else if (credits <= 89)
+            {
+                return "Junior";
+            } else
+            {
+                return "Senior";
+            }
+        }
+
+
+
+
+
         //public Student(string name, int studentId) : this(name, studentId, 0, 0.0) { }
 
         //public Student(string name): this(name, nextStudentId)
