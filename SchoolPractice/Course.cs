61d6f202903f65fedec2c861af084c4b9d6b3a05
Nicholas Pfleegor <ndp5030@gmail.com>
2022-05-05 03:59:13 -0400

    Finished Chapter 4 Exercises

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public List<Student> StudentRoster { get; set; }
+
+        public Course(string name, string teacher, List<Student> studentRoster)
+        {
+            Name = name;
+            Teacher = teacher;
+            StudentRoster = studentRoster;
+        }
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -6,7 +7,24 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
-            // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student nick = new Student("Nick", 1, 1, 4.0);
+            Student jenelle = new Student("Jenelle", 1, 1, 4.0);
+            Student francesca = new Student("Francesca", 1, 1, 4.0);
+            //Console.WriteLine($"The Student class works! \nName: {nick.Name} \nStudent Id: {nick.StudentId} \nCredits: {nick.NumberOfCredits} \nGPA: {nick.Gpa}");
+            List<Student> students = new List<Student>();
+            students.Add(nick);
+            students.Add(jenelle);
+            students.Add(francesca);
+
+            Course codeCamp = new Course("Code Camp 101", "Professor Odegard", students);
+            Console.WriteLine($"Name: {codeCamp.Name}");
+            Console.WriteLine($"Name: {codeCamp.Teacher}");
+            Console.WriteLine("Student Roster");
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"..........\nName: {student.Name} \nStudent Id: {student.StudentId} \nCredits: {student.NumberOfCredits} \nGPA: {student.Gpa}");
+            }
         }
     }
 }
